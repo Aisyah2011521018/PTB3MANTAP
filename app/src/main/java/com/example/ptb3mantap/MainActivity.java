02a8f4d7fa428523a057d411687eb6f778816431
Aisyah2011521018 <91660844+Aisyah2011521018@users.noreply.github.com>
2022-09-17 11:31:55 +0700

    Listlogbook testing

--- app/src/main/java/com/example/ptb3mantap/MainActivity.java
+++ app/src/main/java/com/example/ptb3mantap/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ptb3mantap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickDashboard (View View){
+        Intent A = new Intent(this, ListLogbook.class);
+        startActivity(A);
+    }
+
 }
